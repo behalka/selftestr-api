f3669fff3576dbf5c59f58934ae7a79736c560fd
Karolina Behalova <kaja.behalova@strv.com>
2017-03-18 19:17:52 +0100

    fixed auth middleware pipeline

--- src/middleware/authorization.js
+++ src/middleware/authorization.js
@@ -29,7 +29,7 @@ module.exports = {
     async (ctx, middleware) => {
       const authorization = ctx.headers.authorization
       if (!authorization) {
-        await middleware()
+        return middleware()
       }
       const payload = await parseToken(ctx, authorization)
       ctx.request.user = await setUser(payload)
